8177bd5580adc44777ad74ad8c726281b38cfb45
danny099 <dalmariv01@gmail.com>
2020-02-14 17:41:37 -0500

    init aand engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """file storage"""
+import json
+import os.path
 
 
 class FileStorage:
     """"class file storage"""
     def __init__(self):
         """init function"""
-        __file_path = "./file.json"
+        __file_path = "././file.json"
         __objects = {}
 
     def all(self):
@@ -15,6 +17,15 @@ class FileStorage:
     
     def new(self, obj):
         """sets the obj whith key"""
-        
-
-        
+        self.__objects = self.obj.id
+    
+    def save(self):
+        """serialize"""
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(self.__objects))
+    
+    def reload(self):
+        """deserialice"""
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                self.__objects = json.loads(f)
