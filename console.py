3185b14e5c2998ce45388175ec0a3583a8ffcde2
danny099 <dalmariv01@gmail.com>
2020-02-18 09:49:29 -0500

    executable

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-import cmd, sys
+import cmd
+import sys
+import models
+
 """console"""
 
 
@@ -8,10 +11,25 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_EOF(self, line):
+        'exit'
         return True
 
     def do_quit(self, line):
+        'exit whit command quit'
         return True
+
+    def do_create(self, args):
+        """Creates an instance of BaseModel"""
+        if args:
+            if args in models.list_class:
+                obj = models.base_model.BaseModel()
+                print(obj.id)
+                models.storage.save()
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+        return
 
 
 if __name__ == '__main__':
--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,7 @@
 from models.engine.file_storage import FileStorage
 
 
+list_class = {'BaseModel': 'BaseModel', 'State': 'State', 'Amenity': 'Amenity',
+              'Place': 'Place', 'Review': 'Review', 'User': 'User'}
 storage = FileStorage()
 storage.reload()
