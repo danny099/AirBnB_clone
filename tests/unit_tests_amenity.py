d724ab69c0066be64a8c1b74038a342c7fe7d9c5
francisco0522 <pacho0522@gmail.com>
2020-02-19 00:40:53 -0500

    Adding unit test

---
+++ tests/unit_tests_amenity.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Unit test Amenity"""
+import unittest
+import pep8
+import os
+from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+
+
+class Test(unittest.TestCase):
+    """test"""
+
+    def setUp(self):
+        """set the class"""
+        self.test_class = Amenity()
+
+    def test_doc(self):
+        """test the documentation """
+        self.assertIsNotNone(Amenity.__doc__)
+        self.assertIsNotNone(Amenity.__init__.__doc__)
+
+    def test_name(self):
+        """test the name"""
+        name_test = 'Holberton Test'
+        self.test_class.name = name_test
+        self.assertEqual(self.test_class.name, name_test)
+
+    def test_dict(self):
+        """test the dict"""
+        dict_test = self.test_class.to_dict()
+        self.assertIsInstance(dict_test["created_at"], str)
+        self.assertIsInstance(dict_test["updated_at"], str)
+        self.assertIsInstance(dict_test["id"], str)
+
+    def test_instance(self):
+        """ check if amenity_1 is instance of Amenity """
+        self.assertIsInstance(self.test_class, Amenity)
+        
