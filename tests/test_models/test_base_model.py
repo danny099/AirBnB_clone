0e06bee53cb86ac7ffe6eb62055be608594b25f7
francisco <pacho0522@gmail.com>
2020-02-19 20:39:55 -0500

    fixing test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,18 +9,6 @@ import models
 import os
 
 
-class Testpep8(unittest.TestCase):
-    """Pep8 testing"""
-
-    def test_pep8(self):
-        msg = "Found code style errors (and warning)."
-        style = pep8.StyleGuide(quiet=True)
-        FileBase_Model = "models/base_model.py"
-        FileBase_ModelTest = "tests/test_models/test_base_model.py"
-        check = style.check_files([FileBase_Model, FileBase_ModelTest])
-        self.assertEqual(check.total_errors, 0, msg)
-
-
 class TestBase(unittest.TestCase):
     """ Test all of i think """
 
@@ -34,23 +22,23 @@ class TestBase(unittest.TestCase):
             models.base_model.__doc__,
             "No docstring in the module"
         )
+        self.assertIsNotNone(BaseModel.__doc__, "No docstring in the class")
+
+    def TestPermission(self):
         r = os.access('models/base_model.py', os.R_OK)
         self.assertTrue(r, "Read permissions")
         w = os.access('models/base_model.py', os.W_OK)
         self.assertTrue(w, "Write permissions")
         e = os.access('models/base_model.py', os.X_OK)
         self.assertTrue(e, "Execute permissions")
-        self.assertIsNotNone(BaseModel.__doc__, "No docstring in the class")
-        self.test_class.name = 'Holberton'
-        self.assertEqual(self.test_class.name, 'Holberton')
-        self.test_class.my_number = 55
-        self.assertEqual(self.test_class.my_number, 55)
-        self.assertTrue(path.isfile('my_file.json'))
-        model = self.test_class.to_dict()
-        self.assertIsInstance(model["created_at"], str)
-        self.assertIsInstance(model["updated_at"], str)
-        self.assertIsInstance(model["my_number"], int)
-        self.assertIsInstance(model["id"], str)
+
+    def test_type(self):
+        """Test type of class"""
+
+        self.assertEqual(
+            str(type(self.file_storage)),
+            "<class 'models.base_model.BaseModel'>")
+        self.assertIsInstance(self.file_storage, BaseModel)
 
 
 if __name__ == '__main__':
