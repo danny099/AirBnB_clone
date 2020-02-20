047fc81044468622b0068bcac932b60cefee762c
danny099 <dalmariv01@gmail.com>
2020-02-20 05:16:45 +0000

    prueba

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,49 +1,53 @@
 #!/usr/bin/python3
-"""file storage"""
+"""
+Here you can find the FileStorage Class
+"""
+
 import json
-import os.path
-import models
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
 
-list_class = {"BaseModel": BaseModel, "State": State, "Amenity": Amenity,
-              "Place": Place, "Review": Review, "User": User}
+
+classes = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity,
+           "City": City, "Place": Place, "Review": Review, "State": State}
 
 
 class FileStorage:
-    """"class file storage"""
-    __file_path = "././file.json"
+    """that serializes instances to a JSON file and deserializes
+    JSON file to instances"""
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """return objects"""
+        """returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """sets the obj whith key"""
-        key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
-        """serialize"""
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w', encoding="utf-8") as f:
-            dic = {}
-            for key in self.__objects.keys():
-                dic[key] = self.__objects[key].to_dict()
-            json.dump(dic, f)
+            json.dump(json_objects, f)
 
     def reload(self):
-        """deserialize"""
+        """deserializes the JSON file to __objects
+        (only if the JSON file (__file_path)"""
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as f:
-                json = json.load(f)
-            for key in json:
-                self.__objects[key] = classes[json[key]
-                                              ["__class__"]](**json[key])
+                js = json.load(f)
+            for key in js:
+                self.__objects[key] = classes[js[key]["__class__"]](**js[key])
         except:
             pass
